9bff23e1ae526254888970f1c1493c6c9aa1a1fa
Konstantin Kostov <konstantin@headbright.be>
2017-12-31 12:33:28 +0100

    Remove Atom package boilerplate

--- spec/vue-atom-snippets-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import VueAtomSnippetsView from '../lib/vue-atom-snippets-view';
-
-describe('VueAtomSnippetsView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
