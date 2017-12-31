c199caea1bd94ff90830083de36bfc286a1976a7
Konstantin Kostov <konstantin@headbright.be>
2017-12-31 12:24:32 +0100

    Initial commit

---
+++ spec/vue-atom-snippets-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import VueAtomSnippetsView from '../lib/vue-atom-snippets-view';
+
+describe('VueAtomSnippetsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
